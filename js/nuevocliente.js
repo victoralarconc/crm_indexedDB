924741d8e5d2c396c18bf9f8077d1f76dfb0f929
Víctor Alarcón <victoralarcon3381@gmail.com>
2022-08-19 08:10:20 -0400

    Customer add form

---
+++ js/nuevocliente.js
@@ -0,0 +1,61 @@
+(function () {
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        conectarDB();
+
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        //leer todos los inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        if (nombre === '' || email === '' || telefono === '' || empresa === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+            return;
+        }
+
+        //Crear un objeto con la información
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+        }
+        cliente.id = Date.now();
+
+
+        CrearNuevoCliente(cliente);
+    }
+
+    function CrearNuevoCliente(cliente){
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function(){
+            imprimirAlerta('Hubo un error','error');
+        };
+
+        transaction.oncomplete = function (){
+            //console.log('Cliente agregado');
+            imprimirAlerta('El cliente se agregó correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html'
+            }, 1500);
+        }
+
+    }
+
+})();
